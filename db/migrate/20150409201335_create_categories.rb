f1307d1512312c71ba714b4b4693e2aa40e7e325
lsavoie <twosavoie@gmail.com>
2018-12-11 20:01:09 -0600

    added categories with a new db

--- db/migrate/20150409201335_create_categories.rb
+++ db/migrate/20150409201335_create_categories.rb
@@ -6,16 +6,16 @@ class CreateCategories < ActiveRecord::Migration
 
     add_column :pins, :category_id, :integer, references: :categories
     add_index :pins, :category_id
-    
+
     if Category.all.empty?
       Category.create(name: "ruby")
       Category.create(name: "rails")
       Category.create(name: "unknown")
     end
-    
+
     unknown_category = Category.find_by_name("unknown")
     pins = Pin.where("category_id is null")
-    
+
     pins.each do |pin|
       category = Category.find_by_name(pin.resource_type)
       if category.present?
@@ -25,11 +25,11 @@ class CreateCategories < ActiveRecord::Migration
       end
       pin.save
     end
-    
+
     if Pin.where("category_id is null").empty?
       remove_column :pins, :resource_type
       puts "MIGRATION SUCCESSFUL!"
-      puts "All your data has been migrated successfully."      
+      puts "All your data has been migrated successfully."
     else
       puts "ERROR! Something went wrong - not all pins have been assigned a category Id."
     end
