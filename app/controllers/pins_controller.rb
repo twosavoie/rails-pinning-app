495303d6b1ca70d642d824caecc51c4336fb227c
lsavoie <twosavoie@gmail.com>
2018-12-13 23:11:59 -0600

    added strong parameters

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -13,4 +13,25 @@ class PinsController < ApplicationController
     render :show
   end
 
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.valid?
+      @pin.save
+      redirect_to "/pins/#{@pin.id}" #pin_path(@pin) #:show? "/pins/#{@pin.id}" # HJR & Shana redirect_to @pin AA to pin_path(@pin)
+    else
+      @errors = @pin.errors
+      render :new
+    end
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+  end
+
 end
