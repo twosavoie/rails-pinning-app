e68bf9570f6f27f4deff24951f9d393c13f3da23
lsavoie <twosavoie@gmail.com>
2018-12-15 22:12:23 -0600

    removed diff notes

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -21,7 +21,7 @@ class PinsController < ApplicationController
     @pin = Pin.new(pin_params)
     if @pin.valid?
       @pin.save
-      redirect_to "/pins/#{@pin.id}" #pin_path(@pin) #:show? "/pins/#{@pin.id}" # HJR & Shana redirect_to @pin AA to pin_path(@pin)
+      redirect_to "/pins/#{@pin.id}" 
     else
       @errors = @pin.errors
       render :new
@@ -30,19 +30,12 @@ class PinsController < ApplicationController
 
   def edit
     @pin = Pin.find(params[:id])
-<<<<<<< HEAD
-=======
     render :edit
->>>>>>> forms
   end
 
   def update
     @pin = Pin.find(params[:id])
-<<<<<<< HEAD
     @pin.update(pin_params) #update_attributes dep in R6
-=======
-    @pin.update(pin_params) # update_attributes dep in R6
->>>>>>> forms
     if @pin.valid?
       @pin.save
       redirect_to "/pins/#{@pin.id}"
@@ -50,11 +43,8 @@ class PinsController < ApplicationController
       @errors = @pin.errors
       render :edit
     end
-<<<<<<< HEAD
   end
-=======
-  end 
->>>>>>> forms
+
 
   private
 
